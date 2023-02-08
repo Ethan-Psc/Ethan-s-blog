06760568bb1342ba49e71f9dd2fbb10912d3f707
tanyongjia <897795208@qq.com>
2023-02-08 23:28:23 +0800

    feat(*): fix bug

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@
 
 const siteMetadata = {
   title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  author: 'Ethan',
   headerTitle: 'Ethan`s blog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
